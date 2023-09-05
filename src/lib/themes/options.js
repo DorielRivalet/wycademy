b1a258d6df40041d20c7caed6e34a86b43f3f065
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2023-09-05 01:24:22 -0300

    feat: add images

---
+++ src/lib/themes/options.js
@@ -0,0 +1,7 @@
+// removed white
+export const themeOptions = [
+    { value: "g10", labelText: "Catppuccin Latte",},
+    { value: "g80", labelText: "Catppuccin Frappé",},
+    { value: "g90", labelText: "Catppuccin Macchiato",},
+    { value: "g100", labelText: "Catppuccin Mocha",},
+  ];
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+
 	import Header from './Header.svelte';
 	import './styles.css';
 	import {Button} from 'carbon-components-svelte';
@@ -6,19 +7,42 @@
     Theme,
     RadioButtonGroup,
     RadioButton,
+		Dropdown,
   } from "carbon-components-svelte";
-  import { themeTokens } from '$lib/themes/tokens.ts';
+  import { themeTokens } from '$lib/themes/tokens.js';
+	import { themeOptions } from '$lib/themes/options.js';
+	import type { CarbonTheme } from 'carbon-components-svelte/types/Theme/Theme.svelte';
+	import latteSquircle  from '$lib/images/latte_squircle.png';
+	import macchiatoSquircle  from '$lib/images/macchiato_squircle.png';
 
-  let theme = "g100";
-	let tokens = {};
+  let theme:CarbonTheme = "g100";
+	let tokens = {}; // needed?
 
-	// removed white
-	const themeOptions = [
-    { value: "g10", labelText: "Catppuccin Latte" },
-    { value: "g80", labelText: "Catppuccin Frappé" },
-    { value: "g90", labelText: "Catppuccin Macchiato" },
-    { value: "g100", labelText: "Catppuccin Mocha" },
-  ];
+	function getThemeIcon(themeId: string) {
+		switch (themeId) {
+			case "0":
+				return latteSquircle;
+			default:
+				return macchiatoSquircle;
+		}
+	}
+
+	function changeTheme(event: any){
+		switch (event.selectedId) {
+			case "0":
+				theme = "g10";
+				break;
+			case "1":
+				theme = "g80";
+				break;
+			case "2":
+				theme = "g90";
+				break;
+			case "3":
+				theme = "g100";
+				break;
+		}
+	}
 
 	$: {
 		tokens = themeTokens[theme] || themeTokens.default;
@@ -30,11 +54,31 @@ tokens={tokens}/>
 
 <div class="app">
 	<Header />
-	<RadioButtonGroup legendText="Color theme" bind:selected={theme}>
-		{#each themeOptions as option}
-			<RadioButton labelText={option.labelText} value={option.value} />
-		{/each}
-	</RadioButtonGroup>
+	<div>
+		<RadioButtonGroup legendText="Color theme" bind:selected={theme}>
+			{#each themeOptions as option}
+				<RadioButton labelText={option.labelText} value={option.value} />
+			{/each}
+		</RadioButtonGroup>
+		<Dropdown
+			titleText="Theme"
+			hideLabel
+			selectedId="0"
+			items={[
+				{ id: "0", text: themeOptions[0].labelText,},
+				{ id: "1", text: themeOptions[1].labelText,},
+				{ id: "2", text: themeOptions[2].labelText,},
+				{ id: "3", text: themeOptions[3].labelText,},
+			]}
+			on:select={(event) => changeTheme(event)}
+				let:item
+		>
+		<div>
+			<img alt="Theme Icon" src={getThemeIcon(item.id)} width="24" height="auto"/>
+			<strong>{item.text}</strong>
+		</div>
+		</Dropdown>
+	</div>
 	<main>
 		<slot />	
 	</main>
