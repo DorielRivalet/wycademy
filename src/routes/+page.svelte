c602024537338c858b18748ffbd2ae319c243d8a
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2023-09-29 05:00:50 -0300

    feat: update page metadata

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 <script>
 	import Counter from '$lib/client/components/Counter.svelte';
 	import SectionHeadingTopLevel from '$lib/client/components/SectionHeadingTopLevel.svelte';
-	import logo from '$lib/client/images/logo.png';
+	import logo from '$lib/client/images/logo.svg';
 	import { description, title } from '$lib/constants';
 	console.log('page.svelte');
 </script>
@@ -16,13 +16,14 @@
 	<!--https://stackoverflow.com/questions/59335731/how-to-create-own-embed-site-for-discord-->
 	<title>{title} - Home</title>
 	<meta content="Home" property="og:title" />
+	<link rel="canonical" href="https://frontier-compendium.vercel.app" />
+	<meta content="https://frontier-compendium.vercel.app" property="og:url" />
 	<meta content={description} property="og:description" />
 	<meta name="description" content={description} />
-	<meta content="https://frontier-compendium.vercel.app" property="og:url" />
 	<meta content={logo} property="og:image" />
 	<meta content="#89b4fa" data-react-helmet="true" name="theme-color" />
 	<meta property="og:site_name" content={title} />
-	<link rel="canonical" href="https://frontier-compendium.vercel.app" />
+	<meta property="og:type" content="WebApplication" />
 	<!-- Structured Data
 		<script type="application/ld+json">
 		{
