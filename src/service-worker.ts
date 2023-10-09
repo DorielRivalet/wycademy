74788f96a9e4c3fb411973e4f78273f4b692357e
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2023-10-09 19:25:15 -0300

    feat: update offline page fallback

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,3 +1,11 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+const sw = self as unknown as ServiceWorkerGlobalScope;
+// Create a unique cache name for this deployment
+const CACHE = `cache-${version}`;
+
 // /// <reference types='@sveltejs/kit'/>
 // import { build, files, version } from '$service-worker';
 // import fetchEvent from './fetchEvent';
@@ -10,10 +18,38 @@ import { setDefaultHandler } from 'workbox-routing';
 
 setDefaultHandler(new NetworkOnly());
 
-const precache_list = [...build, ...files, ...prerendered].map((file) => ({
+const precache_list = [
+	...build, // the app itself
+	...files, // everything in static folder
+	...prerendered,
+].map((file) => ({
 	url: file,
 	revision: version,
 }));
 
 precacheAndRoute(precache_list); // this has to run early.
 offlineFallback(); // ONE LINE OF CODE
+
+sw.addEventListener('activate', (event) => {
+	event.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames
+					.filter((cacheName) => {
+						return cacheName !== CACHE;
+					})
+					.map((cacheName) => {
+						return caches.delete(cacheName);
+					}),
+			);
+		}),
+	);
+});
+
+sw.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			return response || fetch(event.request);
+		}),
+	);
+});
