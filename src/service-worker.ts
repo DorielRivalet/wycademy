ae2d88236d175860d62816d31c644fd03ecaf591
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2023-10-09 11:11:38 -0300

    feat: add offline fallback page

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -2,3 +2,18 @@
 // import { build, files, version } from '$service-worker';
 // import fetchEvent from './fetchEvent';
 // import installEvent from './installEvent';
+import { build, files, prerendered, version } from '$service-worker';
+import { offlineFallback } from 'workbox-recipes';
+import { precacheAndRoute } from 'workbox-precaching';
+import { NetworkOnly } from 'workbox-strategies';
+import { setDefaultHandler } from 'workbox-routing';
+
+setDefaultHandler(new NetworkOnly());
+
+const precache_list = [...build, ...files, ...prerendered].map((file) => ({
+	url: file,
+	revision: version,
+}));
+
+precacheAndRoute(precache_list); // this has to run early.
+offlineFallback(); // ONE LINE OF CODE
