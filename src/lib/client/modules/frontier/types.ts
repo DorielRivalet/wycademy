56e04a9b50d02269dd2565a1b1cc95c5437d3843
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2023-10-12 07:44:23 -0300

    feat: add gunlance shell to weapon component

--- src/lib/client/modules/frontier/types.ts
+++ src/lib/client/modules/frontier/types.ts
@@ -70,6 +70,8 @@ export type FrontierWeaponLength =
 	| 'Medium'
 	| 'Long'
 	| 'Very Long';
+export type FrontierGunlanceShell = 'Spread' | 'Long' | 'Normal';
+export type FrontierGunlanceShellLevel = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 export type FrontierWeaponClass = 'Blademaster' | 'Gunner';
 export type FrontierEquipmentRank = '' | 'G' | 'Z';
 export type FrontierWeaponSharpness = [
