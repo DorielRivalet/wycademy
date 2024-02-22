d3389ebacc2eb0c817538c708c3b7a5b6fde1f84
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-02-22 06:56:04 -0300

    build: migrate to netlify

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@
  * found in the LICENSE file.
  */
 
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { optimizeImports } from 'carbon-preprocess-svelte';
 
@@ -17,7 +17,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false,
+		}),
 	},
 };
 
