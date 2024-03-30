b6f16950adcaf361e9f186c68fd4a5a3ec868780
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-03-30 19:50:13 -0300

    feat(arena): update great sword active feature

--- src/lib/client/components/frontier/icon/weapon/Hammer_Icon_White2.svelte
+++ src/lib/client/components/frontier/icon/weapon/Hammer_Icon_White2.svelte
@@ -5,8 +5,10 @@
 	import type { FrontierRarity } from '$lib/client/modules/frontier/types';
 	export let rarity: FrontierRarity = 1;
 	export let size = '100%';
+	export let color = '';
 
-	$: targetColor = RarityColors[rarity - 1] ?? RarityColors[0];
+	$: targetColor =
+		color === '' ? RarityColors[rarity - 1] ?? RarityColors[0] : color;
 </script>
 
 <svg width={size} viewBox="0 0 16.933333 16.933334" version="1.1" id="svg4846"
