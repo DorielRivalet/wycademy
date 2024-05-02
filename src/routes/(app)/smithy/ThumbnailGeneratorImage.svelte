c08cdba8bebc341214a6478e2bf094461818b260
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-05-02 18:24:58 -0300

    feat(smithy): add thumbnail generator image component

--- src/routes/(app)/smithy/ThumbnailGeneratorImage.svelte
+++ src/routes/(app)/smithy/ThumbnailGeneratorImage.svelte
@@ -1,10 +1,233 @@
 <script lang="ts">
-	export let src: string;
-	export let width: string;
-	export let height: string;
-	export let zindex: number;
+	import MagicWand from 'carbon-icons-svelte/lib/MagicWand.svelte';
+	import TrashCan from 'carbon-icons-svelte/lib/TrashCan.svelte';
+	import Button from 'carbon-components-svelte/src/Button/Button.svelte';
+	import Dropdown, {
+		type DropdownItem,
+	} from 'carbon-components-svelte/src/Dropdown/Dropdown.svelte';
+	import Toggle from 'carbon-components-svelte/src/Toggle/Toggle.svelte';
+	import NumberInput from 'carbon-components-svelte/src/NumberInput/NumberInput.svelte';
+	import ColorPicker from 'svelte-awesome-color-picker';
+	import type { FrontierImageType } from '$lib/client/modules/frontier/types';
+	import { getHexStringFromCatppuccinColor } from '$lib/client/themes/catppuccin';
+	import { theme } from '$lib/client/stores/theme';
+	import {
+		ColorCodes,
+		ItemColors,
+		RarityColors,
+	} from '$lib/client/modules/frontier/objects';
+	import CodeSnippet from 'carbon-components-svelte/src/CodeSnippet/CodeSnippet.svelte';
+	import { createEventDispatcher } from 'svelte';
+
+	export let name = 'Name';
+	export let top = 0;
+	export let left = 0;
+	export let width = 256;
+	export let height = 256;
+	export let zindex = 1;
+	export let opacity = 1;
+	export let dropShadowSize: number;
+	export let dropShadowColor: string;
+	export let borderWidth: number;
+	export let borderColor: string;
+	export let borderRadius: number;
+	export let background = true;
+	export let color: '#ffffff';
+	export let monsterRenderSize: 'Small' | 'Full';
+	export let imageType: FrontierImageType | undefined;
+	export let index = 0;
+
+	const dispatch = createEventDispatcher();
+
+	function deleteElement() {
+		// Emit an event to the parent component with the index
+		dispatch('delete', { index });
+	}
+
+	function duplicate() {
+		// Emit an event to the parent component with the index
+		dispatch('duplicate', { index });
+	}
+
+	function getAllFrontierColors() {
+		let result: DropdownItem[] = [];
+		RarityColors.forEach((e, i) => {
+			if (i < 3) {
+				// For the first three entries, group them under "Rare 1-3"
+				if (i === 0) {
+					result.push({ id: e, text: `Rare 1-3` });
+				}
+			} else {
+				// For the rest, use the index to generate the text "Rare 4 to Rare 12"
+				result.push({ id: e, text: `Rare ${i + 1}` });
+			}
+		});
+		ItemColors.forEach((e) => result.push({ id: e.value, text: e.name }));
+		ColorCodes.forEach((e, i) => result.push({ id: e, text: `~C${i}` }));
+
+		// Remove duplicates based on 'id'
+		const uniqueResult = result.filter(
+			(obj, index) => result.findIndex((item) => item.id === obj.id) === index,
+		);
+
+		return uniqueResult;
+	}
+
+	const allFrontierColors = getAllFrontierColors();
 </script>
 
-<div></div>
+<div class="container flex-column">
+	<div class="flex-row">
+		<CodeSnippet code={name} type="inline" hideCopyButton light />
+		<Button kind="danger-tertiary" icon={TrashCan} on:click={deleteElement}
+			>Delete</Button
+		>
+		<Button kind="tertiary" icon={MagicWand} on:click={duplicate}
+			>Duplicate</Button
+		>
+	</div>
+	<div class="flex-row">
+		{#if imageType === 'Armor' || imageType === 'Item' || imageType === 'Weapon'}
+			<Dropdown
+				type="inline"
+				titleText="Color"
+				bind:selectedId={color}
+				items={allFrontierColors}
+			/>
+		{/if}
+		{#if imageType === 'Monster Render'}
+			<Dropdown
+				type="inline"
+				titleText="Render Size"
+				bind:selectedId={monsterRenderSize}
+				items={[
+					{ id: 'Small', text: 'Small' },
+					{ id: 'Full', text: 'Full' },
+				]}
+			/>
+		{/if}
+		{#if imageType === 'Monster Icon'}
+			<Toggle labelText="Background" bind:toggled={background} />
+		{/if}
+		<ColorPicker
+			bind:hex={dropShadowColor}
+			label="Image Shadow Color"
+			--cp-bg-color={getHexStringFromCatppuccinColor('base', $theme)}
+			--cp-border-color={getHexStringFromCatppuccinColor('text', $theme)}
+			--cp-input-color={getHexStringFromCatppuccinColor('surface0', $theme)}
+			--cp-button-hover-color={getHexStringFromCatppuccinColor('blue', $theme)}
+		/>
+		<NumberInput
+			size="sm"
+			step={1}
+			min={0}
+			max={128}
+			bind:value={dropShadowSize}
+			invalidText={'Value must be between 0 and 128'}
+			label={'Image Shadow Width (px)'}
+		/>
+		<ColorPicker
+			bind:hex={borderColor}
+			label="Image Border Color"
+			--cp-bg-color={getHexStringFromCatppuccinColor('base', $theme)}
+			--cp-border-color={getHexStringFromCatppuccinColor('text', $theme)}
+			--cp-input-color={getHexStringFromCatppuccinColor('surface0', $theme)}
+			--cp-button-hover-color={getHexStringFromCatppuccinColor('blue', $theme)}
+		/>
+		<NumberInput
+			size="sm"
+			step={1}
+			min={0}
+			max={128}
+			bind:value={borderWidth}
+			invalidText={'Value must be between 0 and 128'}
+			label={'Image Border Width (px)'}
+		/>
+		<NumberInput
+			size="sm"
+			step={1}
+			min={0}
+			max={128}
+			bind:value={borderRadius}
+			invalidText={'Value must be between 0 and 128'}
+			label={'Image Border Radius (px)'}
+		/>
+		<NumberInput
+			size="sm"
+			step={1}
+			min={0}
+			max={720}
+			bind:value={top}
+			invalidText={'Value must be between 0 and 720'}
+			label={'Top Position (px)'}
+		/>
+		<NumberInput
+			size="sm"
+			step={1}
+			min={0}
+			max={1280}
+			bind:value={left}
+			invalidText={'Value must be between 0 and 1280'}
+			label={'Left Position (px)'}
+		/>
+		<NumberInput
+			size="sm"
+			step={1}
+			min={0}
+			max={2048}
+			bind:value={width}
+			invalidText={'Value must be between 0 and 2048'}
+			label={'Width (px)'}
+		/>
+		<NumberInput
+			size="sm"
+			step={1}
+			min={0}
+			max={2048}
+			bind:value={height}
+			invalidText={'Value must be between 0 and 2048'}
+			label={'Height (px)'}
+		/>
+		<NumberInput
+			size="sm"
+			step={1}
+			min={0}
+			max={100}
+			bind:value={zindex}
+			invalidText={'Value must be between 0 and 100'}
+			label={'ZIndex'}
+		/>
+		<NumberInput
+			size="sm"
+			step={0.1}
+			min={0}
+			max={1}
+			bind:value={opacity}
+			invalidText={'Value must be between 0 and 1'}
+			label={'Opacity'}
+		/>
+	</div>
+</div>
 
-<style lang="scss"></style>
+<style lang="scss">
+	.container {
+		border: 2px solid var(--ctp-overlay0);
+		border-radius: 5px;
+		padding: 1rem;
+		background: var(--ctp-surface0);
+	}
+
+	.flex-row {
+		display: flex;
+		flex-direction: row;
+		gap: 1rem;
+		align-items: center;
+		flex-wrap: wrap;
+	}
+
+	.flex-column {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+</style>
