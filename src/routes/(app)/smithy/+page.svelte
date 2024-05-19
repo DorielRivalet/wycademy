4a726917a41e3ea2b7564a9f866eb5c797c8f795
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-05-19 15:37:34 -0300

    feat(smithy): add font previews

--- src/routes/(app)/smithy/+page.svelte
+++ src/routes/(app)/smithy/+page.svelte
@@ -6109,7 +6109,10 @@
 						{ id: 'Lucida Console', text: 'Lucida Console' },
 						{ id: 'MS Gothic', text: 'MS Gothic' },
 					]}
-				/>
+					let:item
+				>
+					<p style="font-family: {item.id}">{item.id}</p>
+				</Dropdown>
 
 				<Dropdown
 					titleText="Font Style"
