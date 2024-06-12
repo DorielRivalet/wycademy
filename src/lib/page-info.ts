c5033b91a17a263f3df15b3cbe218e361f9ca6e8
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-06-11 23:34:44 -0300

    feat: add monsters to search

--- src/lib/page-info.ts
+++ src/lib/page-info.ts
@@ -1,34 +1,34 @@
-// import type { FrontierMonsterInfo } from '$lib/client/modules/frontier/types';
-// import type { SearchItem } from '$lib/search';
-// import { getUniqueMonsters } from '$lib/client/modules/frontier/functions';
+import type { FrontierMonsterInfo } from '$lib/client/modules/frontier/types';
+import type { SearchItem } from '$lib/search';
+import { getUniqueMonsters } from '$lib/client/modules/frontier/functions';
 
-// type PossibleArrays = FrontierMonsterInfo;
+type PossibleArrays = FrontierMonsterInfo;
 
-// function mapToSearchItem(
-// 	input: Array<PossibleArrays>,
-// 	name: string,
-// ): SearchItem[] {
-// 	const result: SearchItem[] = [];
-// 	switch (name) {
-// 		case 'monsterInfo':
-// 			input.map(({ displayName, link, ecology, id }) => {
-// 				result.push({
-// 					title: displayName,
-// 					slug: link!,
-// 					content: ecology!,
-// 					id: id,
-// 					hex: `0x${id.toString(16).toUpperCase().padStart(2, '0')}`,
-// 					category: 'Monster',
-// 				});
-// 			});
-// 			break;
-// 		default:
-// 			return [];
-// 	}
+function mapToSearchItem(
+	input: Array<PossibleArrays>,
+	name: string,
+): SearchItem[] {
+	const result: SearchItem[] = [];
+	switch (name) {
+		case 'monsterInfo':
+			input.map(({ displayName, link, ecology, id }) => {
+				result.push({
+					title: displayName,
+					slug: link!,
+					content: ecology!,
+					id: id,
+					hex: `0x${id.toString(16).toUpperCase().padStart(2, '0')}`,
+					category: 'Monster',
+				});
+			});
+			break;
+		default:
+			return [];
+	}
 
-// 	return result;
-// }
+	return result;
+}
 
-// const uniqueMonsters = getUniqueMonsters();
+const uniqueMonsters = getUniqueMonsters();
 
-// export const pageInfo = [...mapToSearchItem(uniqueMonsters, 'monsterInfo')];
+export const pageInfo = [...mapToSearchItem(uniqueMonsters, 'monsterInfo')];
