c20b7b196ad30f3dbfb8b0fd5521fd44e884311e
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-06-11 23:34:44 -0300

    feat: add monsters to search

--- src/routes/api/search/+server.ts
+++ src/routes/api/search/+server.ts
@@ -1,8 +1,9 @@
 import { json } from '@sveltejs/kit';
 import results from './results.json';
+import { pageInfo } from '$lib/page-info';
 
 export const prerender = true;
 
 export async function GET() {
-	return json(results);
+	return json([...results, pageInfo]);
 }
