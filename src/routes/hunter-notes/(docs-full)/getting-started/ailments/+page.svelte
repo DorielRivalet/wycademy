22389916fc3be657ee4cc208cec7fcb43296d58c
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-06-22 06:58:06 -0300

    feat: migrate arena content to hunter notes pages

--- src/routes/hunter-notes/(docs-full)/getting-started/ailments/+page.svelte
+++ src/routes/hunter-notes/(docs-full)/getting-started/ailments/+page.svelte
@@ -2,124 +2,284 @@
 	import PageTurn from '$lib/client/components/PageTurn.svelte';
 	import SectionHeadingTopLevel from '$lib/client/components/SectionHeadingTopLevel.svelte';
 	import HunterNotesPage from '$lib/client/components/HunterNotesPage.svelte';
+	import DataTable from 'carbon-components-svelte/src/DataTable/DataTable.svelte';
+	import InlineTooltip from '$lib/client/components/frontier/InlineTooltip.svelte';
+	import {
+		StatusIcons,
+		monsterInfo,
+	} from '$lib/client/modules/frontier/objects';
+	import Toolbar from 'carbon-components-svelte/src/DataTable/Toolbar.svelte';
+	import CopyButton from 'carbon-components-svelte/src/CopyButton/CopyButton.svelte';
+	import SectionHeading from '$lib/client/components/SectionHeading.svelte';
+	import { getCSVFromArray } from '$lib/client/modules/csv';
+
+	function getStatusArray(input: string) {
+		const values = input.split(',');
+		return values;
+	}
 </script>
 
-<HunterNotesPage displayTOC={true}>
+<HunterNotesPage displayTOC={false}>
 	<div>
 		<SectionHeadingTopLevel title={'Ailments'} />
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Enim diam vulputate ut
-			pharetra sit amet aliquam id diam. In arcu cursus euismod quis viverra
-			nibh cras pulvinar mattis. Fames ac turpis egestas integer. Cursus turpis
-			massa tincidunt dui ut ornare lectus sit. Phasellus egestas tellus rutrum
-			tellus pellentesque eu tincidunt. A iaculis at erat pellentesque. Nulla
-			facilisi etiam dignissim diam quis. Eget est lorem ipsum dolor sit amet
-			consectetur adipiscing elit. Nulla pellentesque dignissim enim sit amet
-			venenatis urna cursus. Eget lorem dolor sed viverra ipsum nunc aliquet
-			bibendum. Mi ipsum faucibus vitae aliquet. Bibendum ut tristique et
-			egestas quis ipsum suspendisse ultrices gravida. Aliquam id diam maecenas
-			ultricies mi eget mauris. Vulputate ut pharetra sit amet aliquam id diam.
-			Dignissim diam quis enim lobortis scelerisque fermentum. Nunc pulvinar
-			sapien et ligula ullamcorper malesuada proin libero. Elit scelerisque
-			mauris pellentesque pulvinar pellentesque habitant morbi tristique
-			senectus. Bibendum ut tristique et egestas quis ipsum suspendisse
-			ultrices. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet.
-			Pretium viverra suspendisse potenti nullam ac. Facilisis gravida neque
-			convallis a cras semper auctor neque vitae. Id volutpat lacus laoreet non
-			curabitur gravida. Elementum eu facilisis sed odio morbi quis commodo
-			odio. Lectus sit amet est placerat in egestas erat imperdiet. Sed
-			ullamcorper morbi tincidunt ornare massa eget egestas. Sit amet dictum sit
-			amet justo donec enim. Id aliquet risus feugiat in ante. Vestibulum
-			rhoncus est pellentesque elit. Et malesuada fames ac turpis egestas
-			maecenas. Laoreet sit amet cursus sit amet. Tincidunt tortor aliquam nulla
-			facilisi cras fermentum odio eu. Placerat duis ultricies lacus sed turpis
-			tincidunt id aliquet risus. Adipiscing at in tellus integer feugiat. Justo
-			nec ultrices dui sapien eget mi proin sed libero. Imperdiet proin
-			fermentum leo vel orci porta non pulvinar neque. Platea dictumst
-			vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Adipiscing
-			bibendum est ultricies integer. Eu scelerisque felis imperdiet proin
-			fermentum leo. Cursus in hac habitasse platea dictumst quisque sagittis
-			purus sit. Mauris augue neque gravida in fermentum et sollicitudin ac
-			orci. Id diam maecenas ultricies mi eget mauris pharetra et ultrices. Nec
-			ultrices dui sapien eget mi. Mollis nunc sed id semper risus. Sit amet
-			aliquam id diam. Quis varius quam quisque id diam vel quam elementum. Urna
-			nec tincidunt praesent semper feugiat nibh. Feugiat nisl pretium fusce id
-			velit ut tortor pretium viverra. Lectus vestibulum mattis ullamcorper
-			velit sed. Magna ac placerat vestibulum lectus mauris ultrices. Euismod in
-			pellentesque massa placerat. Condimentum id venenatis a condimentum vitae
-			sapien. Sed sed risus pretium quam vulputate dignissim suspendisse in.
-			Nulla porttitor massa id neque aliquam vestibulum. Mi sit amet mauris
-			commodo quis imperdiet massa tincidunt nunc. Fermentum odio eu feugiat
-			pretium nibh. Augue interdum velit euismod in pellentesque massa placerat.
-			Id nibh tortor id aliquet lectus proin nibh nisl. Et leo duis ut diam quam
-			nulla porttitor massa. Leo in vitae turpis massa sed elementum. Metus
-			vulputate eu scelerisque felis imperdiet. Cras sed felis eget velit.
-			Lobortis scelerisque fermentum dui faucibus in ornare quam. Et ultrices
-			neque ornare aenean euismod elementum nisi quis eleifend. Pretium quam
-			vulputate dignissim suspendisse in est ante in nibh. Tellus at urna
-			condimentum mattis pellentesque. Egestas sed tempus urna et pharetra
-			pharetra massa massa ultricies. Platea dictumst vestibulum rhoncus est
-			pellentesque elit ullamcorper dignissim. Massa sed elementum tempus
-			egestas. Ornare arcu odio ut sem nulla pharetra. Feugiat pretium nibh
-			ipsum consequat. Commodo elit at imperdiet dui accumsan sit amet nulla.
-			Dolor purus non enim praesent elementum facilisis leo vel. Quam quisque id
-			diam vel quam elementum pulvinar. Porttitor lacus luctus accumsan tortor
-			posuere ac ut consequat. Pellentesque habitant morbi tristique senectus.
-			Dictum sit amet justo donec enim diam vulputate ut pharetra. Volutpat
-			lacus laoreet non curabitur. Pretium nibh ipsum consequat nisl vel pretium
-			lectus quam id. Ac odio tempor orci dapibus ultrices in iaculis. Quam
-			vulputate dignissim suspendisse in est ante. Volutpat diam ut venenatis
-			tellus. Elementum sagittis vitae et leo duis. Netus et malesuada fames ac
-			turpis egestas maecenas pharetra convallis. Diam vel quam elementum
-			pulvinar etiam non quam. Orci porta non pulvinar neque laoreet
-			suspendisse. Nisl condimentum id venenatis a condimentum vitae.
-			Consectetur adipiscing elit duis tristique sollicitudin. Vulputate
-			dignissim suspendisse in est ante in nibh mauris. Laoreet id donec
-			ultrices tincidunt arcu non sodales neque. Convallis aenean et tortor at
-			risus viverra adipiscing at in. Nibh tellus molestie nunc non blandit
-			massa enim nec dui. Curabitur vitae nunc sed velit dignissim sodales. Sed
-			euismod nisi porta lorem mollis aliquam. Eros donec ac odio tempor orci.
-			Sem fringilla ut morbi tincidunt augue. Sit amet justo donec enim. Felis
-			imperdiet proin fermentum leo vel orci porta. Praesent elementum facilisis
-			leo vel fringilla est ullamcorper eget. Condimentum vitae sapien
-			pellentesque habitant morbi tristique. Vitae congue mauris rhoncus aenean
-			vel elit scelerisque mauris pellentesque. Pharetra sit amet aliquam id
-			diam. Pellentesque adipiscing commodo elit at imperdiet. Commodo elit at
-			imperdiet dui accumsan sit amet nulla facilisi. Sagittis nisl rhoncus
-			mattis rhoncus. Vitae semper quis lectus nulla. Potenti nullam ac tortor
-			vitae purus. Id aliquet risus feugiat in ante. Non blandit massa enim nec
-			dui nunc mattis. In massa tempor nec feugiat nisl pretium. Etiam non quam
-			lacus suspendisse faucibus interdum. Malesuada fames ac turpis egestas
-			maecenas pharetra. Ac feugiat sed lectus vestibulum mattis. Pellentesque
-			sit amet porttitor eget dolor. Enim facilisis gravida neque convallis a
-			cras semper. Rhoncus aenean vel elit scelerisque. Volutpat commodo sed
-			egestas egestas. Sed enim ut sem viverra aliquet eget. Vulputate sapien
-			nec sagittis aliquam malesuada bibendum arcu. Dignissim cras tincidunt
-			lobortis feugiat vivamus at augue. Amet nulla facilisi morbi tempus
-			iaculis urna id volutpat. Ornare quam viverra orci sagittis eu volutpat.
-			Iaculis eu non diam phasellus vestibulum lorem. Proin sed libero enim sed
-			faucibus turpis in eu mi. Fermentum odio eu feugiat pretium nibh ipsum
-			consequat. Amet cursus sit amet dictum sit amet justo donec. Cursus sit
-			amet dictum sit amet justo donec enim diam. Duis ultricies lacus sed
-			turpis tincidunt id aliquet. Posuere lorem ipsum dolor sit amet
-			consectetur adipiscing elit. Ut etiam sit amet nisl purus in mollis nunc.
-			Sit amet purus gravida quis blandit turpis. Elementum eu facilisis sed
-			odio. Vitae aliquet nec ullamcorper sit amet risus nullam eget felis. Mi
-			tempus imperdiet nulla malesuada pellentesque elit eget. Massa sed
-			elementum tempus egestas. Vulputate eu scelerisque felis imperdiet proin
-			fermentum leo vel orci. Eget felis eget nunc lobortis. Justo eget magna
-			fermentum iaculis eu non diam phasellus vestibulum. At imperdiet dui
-			accumsan sit amet. Risus nullam eget felis eget nunc lobortis mattis
-			aliquam faucibus. Eget nullam non nisi est sit amet facilisis. Non tellus
-			orci ac auctor augue mauris augue neque gravida. Faucibus interdum posuere
-			lorem ipsum dolor sit amet consectetur adipiscing. Commodo quis imperdiet
-			massa tincidunt nunc. Facilisi morbi tempus iaculis urna id volutpat. Id
-			consectetur purus ut faucibus pulvinar. Leo in vitae turpis massa sed
-			elementum tempus egestas sed. Ullamcorper dignissim cras tincidunt
-			lobortis feugiat vivamus at. Nullam non nisi est sit amet facilisis.
-			Tempor orci dapibus ultrices in.
-		</p>
+		<!-- TODO: status effects descriptions and cures-->
+		<section>
+			<SectionHeading level={2} title="Monster Status Immunities" />
+			<div>
+				<div class="datatable-container">
+					<DataTable
+						sortable
+						useStaticWidth
+						zebra
+						size="short"
+						headers={[
+							{ key: 'name', value: 'Name', minWidth: '1rem' },
+							{ key: 'immunities', value: 'Immunities', minWidth: '1rem' },
+						]}
+						rows={[
+							{
+								id: '1',
+								immunities: 'Paralysis',
+								name: 'Kirin',
+							},
+							{
+								id: '2',
+								immunities: 'Poison,Paralysis,Sleep',
+								name: 'Lao-Shan Lung',
+							},
+							{
+								id: '3',
+								immunities: 'Poison',
+								name: 'Yian Garuga',
+							},
+							{
+								id: '4',
+								immunities: 'Poison,Paralysis,Sleep',
+								name: 'Shen Gaoren',
+							},
+							{
+								id: '5',
+								immunities: 'Poison,Paralysis,Sleep',
+								name: 'Yama Tsukami',
+							},
+							{
+								id: '6',
+								immunities: 'Poison',
+								name: 'Espinas',
+							},
+							{
+								id: '7',
+								immunities: 'Poison,Paralysis,Sleep',
+								name: 'Akura Vashimu',
+							},
+
+							{
+								id: '8',
+								immunities: 'Poison,Paralysis,Sleep',
+								name: 'Akura Jebia',
+							},
+							{
+								id: '9',
+								immunities: 'Paralysis,Sleep',
+								name: 'Berukyurosu',
+							},
+							{
+								id: '10',
+								immunities: 'Poison,Paralysis,Sleep',
+								name: 'Pariapuria',
+							},
+							{
+								id: '11',
+								immunities: 'Poison,Paralysis,Sleep,Blast',
+								name: 'Raviente',
+							},
+							{
+								id: '12',
+								immunities: 'Paralysis,Sleep',
+								name: 'Doragyurosu',
+							},
+							{
+								id: '13',
+								immunities: 'Poison,Paralysis',
+								name: 'Baruragaru',
+							},
+							{
+								id: '14',
+								immunities: 'Sleep',
+								name: 'Inagami',
+							},
+							{
+								id: '15',
+								immunities: 'Poison,Paralysis,Sleep',
+								name: 'Duremudira',
+							},
+							{
+								id: '16',
+								immunities: 'Sleep',
+								name: 'Varusaburosu',
+							},
+							{
+								id: '17',
+								immunities: 'Poison',
+								name: 'Gasurabazura',
+							},
+							{
+								id: '18',
+								immunities: 'Poison,Paralysis,Sleep',
+								name: 'Yama Kurai',
+							},
+							{
+								id: '19',
+								immunities: 'Sleep',
+								name: 'Keoaruboru',
+							},
+						]}
+						><Toolbar
+							><div class="toolbar">
+								<CopyButton
+									iconDescription={'Copy as CSV'}
+									text={getCSVFromArray([
+										{
+											id: '1',
+											immunities: 'Paralysis',
+											name: 'Kirin',
+										},
+										{
+											id: '2',
+											immunities: 'Poison,Paralysis,Sleep',
+											name: 'Lao-Shan Lung',
+										},
+										{
+											id: '3',
+											immunities: 'Poison',
+											name: 'Yian Garuga',
+										},
+										{
+											id: '4',
+											immunities: 'Poison,Paralysis,Sleep',
+											name: 'Shen Gaoren',
+										},
+										{
+											id: '5',
+											immunities: 'Poison,Paralysis,Sleep',
+											name: 'Yama Tsukami',
+										},
+										{
+											id: '6',
+											immunities: 'Poison',
+											name: 'Espinas',
+										},
+										{
+											id: '7',
+											immunities: 'Poison,Paralysis,Sleep',
+											name: 'Akura Vashimu',
+										},
+
+										{
+											id: '8',
+											immunities: 'Poison,Paralysis,Sleep',
+											name: 'Akura Jebia',
+										},
+										{
+											id: '9',
+											immunities: 'Paralysis,Sleep',
+											name: 'Berukyurosu',
+										},
+										{
+											id: '10',
+											immunities: 'Poison,Paralysis,Sleep',
+											name: 'Pariapuria',
+										},
+										{
+											id: '11',
+											immunities: 'Poison,Paralysis,Sleep,Blast',
+											name: 'Raviente',
+										},
+										{
+											id: '12',
+											immunities: 'Paralysis,Sleep',
+											name: 'Doragyurosu',
+										},
+										{
+											id: '13',
+											immunities: 'Poison,Paralysis',
+											name: 'Baruragaru',
+										},
+										{
+											id: '14',
+											immunities: 'Sleep',
+											name: 'Inagami',
+										},
+										{
+											id: '15',
+											immunities: 'Poison,Paralysis,Sleep',
+											name: 'Duremudira',
+										},
+										{
+											id: '16',
+											immunities: 'Sleep',
+											name: 'Varusaburosu',
+										},
+										{
+											id: '17',
+											immunities: 'Poison',
+											name: 'Gasurabazura',
+										},
+										{
+											id: '18',
+											immunities: 'Poison,Paralysis,Sleep',
+											name: 'Yama Kurai',
+										},
+										{
+											id: '19',
+											immunities: 'Sleep',
+											name: 'Keoaruboru',
+										},
+									])}
+								/>
+							</div>
+						</Toolbar>
+						<svelte:fragment slot="cell" let:cell>
+							{#if cell.key === 'name'}
+								<InlineTooltip
+									icon={monsterInfo.find((e) => e.name === cell.value)?.icon}
+									tooltip={'Monster'}
+									iconType={'file'}
+									text={cell.value}
+								/>
+							{:else if cell.key === 'immunities'}
+								{#each getStatusArray(cell.value) as status}
+									<InlineTooltip
+										icon={StatusIcons.find((e) => e.name === status)?.icon ??
+											''}
+										tooltip={'Status'}
+										text={`${status}`}
+									/>
+								{/each}
+							{:else}
+								<p>{cell.value}</p>
+							{/if}
+						</svelte:fragment>
+					</DataTable>
+				</div>
+				<p>
+					Subspecies and Zeniths are also immune, except <InlineTooltip
+						tooltip="Monster"
+						text="Doragyurosu"
+						icon={monsterInfo.find((e) => e.name === 'Doragyurosu')?.icon}
+						iconType={'file'}
+					/> which is immune to these statuses only in G Rank, and also as a Zenith.
+				</p>
+				<p>
+					Only support can inflict poison to <InlineTooltip
+						tooltip="Monster"
+						text="Raviente"
+						iconType={'file'}
+						icon={monsterInfo.find((e) => e.name === 'Raviente')?.icon}
+					/>.
+				</p>
+			</div>
+		</section>
 		<div class="page-turn">
 			<PageTurn
 				previous="/hunter-notes/getting-started/elements"
@@ -134,5 +294,19 @@
 <style lang="scss">
 	.page-turn {
 		margin-top: 4rem;
+	}
+
+	.datatable-container {
+		margin-top: 2rem;
+		margin-bottom: 2rem;
+	}
+
+	.toolbar {
+		padding: 1rem;
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		flex-grow: 1;
+		flex-shrink: 1;
 	}
 </style>
--- src/routes/hunter-notes/(docs-full)/monsters/exotics/+page.svelte
+++ src/routes/hunter-notes/(docs-full)/monsters/exotics/+page.svelte
@@ -1,1 +1,218 @@
-Content.
+<script lang="ts">
+	import PageTurn from '$lib/client/components/PageTurn.svelte';
+	import SectionHeadingTopLevel from '$lib/client/components/SectionHeadingTopLevel.svelte';
+	import HunterNotesPage from '$lib/client/components/HunterNotesPage.svelte';
+	import InlineTooltip from '$lib/client/components/frontier/InlineTooltip.svelte';
+	import { monsterInfo } from '$lib/client/modules/frontier/objects';
+
+	import CopyButton from 'carbon-components-svelte/src/CopyButton/CopyButton.svelte';
+	import Toolbar from 'carbon-components-svelte/src/DataTable/Toolbar.svelte';
+	import DataTable from 'carbon-components-svelte/src/DataTable/DataTable.svelte';
+	import { getCSVFromArray } from '$lib/client/modules/csv';
+</script>
+
+<HunterNotesPage displayTOC={false}>
+	<div>
+		<SectionHeadingTopLevel title={'Exotics'} />
+		<div>
+			<p>
+				Only 1 armor piece is needed to activate the armor skill. All weapons
+				give Speed Eating. Each Exotic monster has a set of associated G Finesse
+				weapon.
+			</p>
+			<div class="exotics-table">
+				<DataTable
+					sortable
+					useStaticWidth
+					zebra
+					size="short"
+					headers={[
+						{ key: 'name', value: 'Name', minWidth: '1rem' },
+						{ key: 'armorSkill', value: 'Automatic Skill', minWidth: '1rem' },
+					]}
+					rows={[
+						{
+							id: 'Zinogre',
+							armorSkill: 'Thunder Attack (Large)',
+							name: 'Zinogre',
+						},
+						{
+							id: 'Deviljho',
+							armorSkill: 'Starving Wolf+2',
+							name: 'Deviljho',
+						},
+						{
+							id: 'Brachydios',
+							armorSkill: 'Bomber',
+							name: 'Brachydios',
+						},
+
+						{
+							id: 'Barioth',
+							armorSkill: 'Ice Attack (Large)',
+							name: 'Barioth',
+						},
+						{
+							id: 'Uragaan',
+							armorSkill: 'Guard+2 / Ammo Combiner',
+							name: 'Uragaan',
+						},
+						{
+							id: 'Nargacuga',
+							armorSkill: 'Evasion+2',
+							name: 'Nargacuga',
+						},
+						{
+							id: 'Stygian Zinogre',
+							armorSkill: 'Dragon Attack (Large)',
+							name: 'Stygian Zinogre',
+						},
+						{
+							id: 'Gore Magala',
+							armorSkill: 'Focus+2',
+							name: 'Gore Magala',
+						},
+						{
+							id: 'Shagaru Magala',
+							armorSkill: 'Status Immunity (Myriad)',
+							name: 'Shagaru Magala',
+						},
+						{
+							id: 'Amatsu',
+							armorSkill: 'Water Attack (Large)',
+							name: 'Amatsu',
+						},
+						{
+							id: 'Seregios',
+							armorSkill: 'Critical Eye+5',
+							name: 'Seregios',
+						},
+					]}
+					><Toolbar
+						><div class="toolbar">
+							<CopyButton
+								iconDescription={'Copy as CSV'}
+								text={getCSVFromArray([
+									{
+										id: 'Zinogre',
+										armorSkill: 'Thunder Attack (Large)',
+										name: 'Zinogre',
+									},
+									{
+										id: 'Deviljho',
+										armorSkill: 'Starving Wolf+2',
+										name: 'Deviljho',
+									},
+									{
+										id: 'Brachydios',
+										armorSkill: 'Bomber',
+										name: 'Brachydios',
+									},
+
+									{
+										id: 'Barioth',
+										armorSkill: 'Ice Attack (Large)',
+										name: 'Barioth',
+									},
+									{
+										id: 'Uragaan',
+										armorSkill: 'Guard+2 / Ammo Combiner',
+										name: 'Uragaan',
+									},
+									{
+										id: 'Nargacuga',
+										armorSkill: 'Evasion+2',
+										name: 'Nargacuga',
+									},
+									{
+										id: 'Stygian Zinogre',
+										armorSkill: 'Dragon Attack (Large)',
+										name: 'Stygian Zinogre',
+									},
+									{
+										id: 'Gore Magala',
+										armorSkill: 'Focus+2',
+										name: 'Gore Magala',
+									},
+									{
+										id: 'Shagaru Magala',
+										armorSkill: 'Status Immunity (Myriad)',
+										name: 'Shagaru Magala',
+									},
+									{
+										id: 'Amatsu',
+										armorSkill: 'Water Attack (Large)',
+										name: 'Amatsu',
+									},
+									{
+										id: 'Seregios',
+										armorSkill: 'Critical Eye+5',
+										name: 'Seregios',
+									},
+								])}
+							/>
+						</div>
+					</Toolbar>
+					<span slot="title">
+						<div class="data-table-title">
+							<div>Exotic Skills</div>
+						</div>
+					</span>
+					<svelte:fragment slot="cell" let:cell>
+						{#if cell.key === 'name'}
+							<InlineTooltip
+								icon={monsterInfo.find((e) => e.name === cell.value)?.icon ??
+									''}
+								iconType="file"
+								tooltip={cell.value}
+								text={cell.value}
+							/>
+						{:else}
+							<p>
+								{cell.value}
+							</p>
+						{/if}
+					</svelte:fragment>
+				</DataTable>
+			</div>
+
+			<div class="page-turn">
+				<PageTurn
+					previous="/hunter-notes/monsters/"
+					previousTitle="Monsters"
+					next="/hunter-notes/monsters/origin"
+					nextTitle="Monsters: Origin"
+				/>
+			</div>
+		</div>
+	</div></HunterNotesPage
+>
+
+<style lang="scss">
+	.page-turn {
+		margin-top: 4rem;
+	}
+
+	.data-table-title {
+		display: flex;
+		gap: 2rem;
+		align-items: center;
+	}
+
+	.exotics-table {
+		margin-top: 2rem;
+		display: flex;
+		flex-wrap: wrap;
+		overflow-x: auto;
+		max-width: 90vw;
+	}
+
+	.toolbar {
+		padding: 1rem;
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		flex-grow: 1;
+		flex-shrink: 1;
+	}
+</style>
