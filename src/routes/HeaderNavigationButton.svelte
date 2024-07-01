4ab15f3ec33f89ee54b83972b429fef5f92791c8
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-07-01 14:25:36 -0300

    feat: update header navigation

---
+++ src/routes/HeaderNavigationButton.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Button from 'carbon-components-svelte/src/Button/Button.svelte';
+	export let path: string;
+	export let description: string;
+</script>
+
+<li>
+	<Button as let:props kind="ghost" iconDescription="Leaderboards">
+		<a href={path}
+			><p {...props}>
+				<button class="button">
+					<span>{description}</span>
+				</button>
+			</p></a
+		>
+	</Button>
+</li>
+
+<style lang="scss">
+	@use '@carbon/type' as type;
+
+	button {
+		all: unset;
+	}
+
+	.button {
+		display: flex;
+		flex-direction: row;
+		color: var(--ctp-text);
+		align-items: center;
+		gap: 0.5rem;
+		@include type.type-style('body-compact-02');
+	}
+</style>
