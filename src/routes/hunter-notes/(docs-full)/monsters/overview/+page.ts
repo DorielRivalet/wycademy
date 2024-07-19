1dcdf108a74e3a4159911a092d48f5390dce0328
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-07-19 20:25:48 -0300

    fix: dont prerender hunter notes pages

--- src/routes/hunter-notes/(docs-full)/monsters/overview/+page.ts
+++ src/routes/hunter-notes/(docs-full)/monsters/overview/+page.ts
@@ -7,4 +7,4 @@ console.log('page.ts');
 
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-export const prerender = true;
+export const prerender = false;
