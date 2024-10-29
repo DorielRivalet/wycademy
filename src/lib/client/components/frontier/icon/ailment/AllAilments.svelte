ae472799736668867fc5b8978dbd056655386d4d
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-10-29 11:03:51 -0300

    feat: update page card icons

---
+++ src/lib/client/components/frontier/icon/ailment/AllAilments.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import {
+		AilmentIcons,
+		getAilmentIcon,
+	} from '$lib/client/modules/frontier/ailments';
+	import { onMount } from 'svelte';
+
+	const content = AilmentIcons.filter((e) => e.icon !== '').map((e) => {
+		return getAilmentIcon(e.name);
+	});
+
+	let randomSelection = getAilmentIcon('Stun');
+
+	export let delay = 2000;
+
+	// Function to select a random item from an array
+	function getRandomItem(arr: any[]) {
+		return arr[Math.floor(Math.random() * arr.length)];
+	}
+
+	// Initialize randomWeapon and randomColor
+	randomSelection = getRandomItem(content);
+
+	// Use onMount to start changing the weapon every 1 second after the component mounts
+	onMount(() => {
+		const intervalId = setInterval(() => {
+			randomSelection = getRandomItem(content);
+		}, delay); // Change every 1 second
+
+		// Cleanup function to clear the interval when the component is destroyed
+		return () => clearInterval(intervalId);
+	});
+</script>
+
+<div>
+	<!-- Pass the random color as a prop to the randomly selected weapon component -->
+	<svelte:component this={randomSelection} />
+</div>
+
+<style lang="scss"></style>
---
+++ src/lib/client/components/frontier/icon/element/AllElements.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import {
+		ElementIcons,
+		getElementIcon,
+	} from '$lib/client/modules/frontier/elements';
+	import { onMount } from 'svelte';
+
+	const content = ElementIcons.filter((e) => e.icon !== '').map((e) => {
+		return getElementIcon(e.name);
+	});
+
+	let randomSelection = getElementIcon('Fire');
+
+	export let delay = 2000;
+
+	// Function to select a random item from an array
+	function getRandomItem(arr: any[]) {
+		return arr[Math.floor(Math.random() * arr.length)];
+	}
+
+	// Initialize randomWeapon and randomColor
+	randomSelection = getRandomItem(content);
+
+	// Use onMount to start changing the weapon every 1 second after the component mounts
+	onMount(() => {
+		const intervalId = setInterval(() => {
+			randomSelection = getRandomItem(content);
+		}, delay); // Change every 1 second
+
+		// Cleanup function to clear the interval when the component is destroyed
+		return () => clearInterval(intervalId);
+	});
+</script>
+
+<div>
+	<!-- Pass the random color as a prop to the randomly selected weapon component -->
+	<svelte:component this={randomSelection} />
+</div>
+
+<style lang="scss"></style>
