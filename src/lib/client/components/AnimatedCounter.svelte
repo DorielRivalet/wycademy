874591c907f64fa30d6f4ff0b8d4f2ad4aa6ff40
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-11-02 16:50:31 -0300

    feat: add home page components

---
+++ src/lib/client/components/AnimatedCounter.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let value: number;
+	export let text: string;
+	export let color: string;
+	export let hoverColor: string = '#ff0000';
+</script>
+
+<div class="container">
+	<div class="icon"><slot /></div>
+	<p class="value" style:color>{value}</p>
+	<p class="text">{text}</p>
+</div>
+
+<style lang="scss">
+	@use '@carbon/type' as type;
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 0.25rem;
+		@include type.type-style('code-02');
+	}
+
+	.icon {
+		padding: 1rem;
+		background-color: var(--ctp-surface0);
+		border: 2px solid var(--ctp-surface1);
+		border-radius: 100%;
+	}
+
+	.value {
+		font-size: 1.5em;
+	}
+</style>
