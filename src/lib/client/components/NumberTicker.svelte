a118e66abf28e859e01c9eb61063e974aaf90e5b
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-11-05 17:42:47 -0300

    feat: update home page

---
+++ src/lib/client/components/NumberTicker.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { cubicOut } from 'svelte/easing';
+	import { onMount } from 'svelte';
+	import { tweened } from 'svelte/motion';
+
+	export let value;
+	export let initial = 0;
+	export let duration = 6000;
+	let num = tweened(initial, {
+		duration: duration,
+		easing: cubicOut,
+	});
+
+	onMount(() => {
+		num.set(value);
+	});
+</script>
+
+<div class="number-ticker">
+	{$num.toFixed(0)}+
+</div>
+
+<style lang="scss">
+	.number-ticker {
+		display: inline-block;
+		font-feature-settings: 'normal';
+	}
+</style>
