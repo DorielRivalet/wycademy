36c54d7f4dcf8f47dadd31a63f0b7237ee5d244a
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2024-11-12 01:02:01 -0300

    perf: update github fetching

--- src/routes/(home)/HomeBlogSectionArticle.svelte
+++ src/routes/(home)/HomeBlogSectionArticle.svelte
@@ -1,48 +1,13 @@
 <script lang="ts">
 	import ClickableTile from 'carbon-components-svelte/src/Tile/ClickableTile.svelte';
 	import Card3D from '$lib/client/components/Card3D.svelte';
+	import { formatDateWithRelativeTime } from '$lib/client/modules/time';
 
 	export let imageSource: string;
 	export let date: string;
 	export let title: string;
 	export let summary: string;
 	export let href: string;
-
-	function formatDateWithRelativeTime(date1: Date, date2: Date): string {
-		const diffInMs = date2.getTime() - date1.getTime();
-		const diffInSeconds = Math.floor(diffInMs / 1000);
-		let relativeTime: string;
-
-		if (diffInSeconds < 60) {
-			relativeTime = `${diffInSeconds}s ago`;
-		} else if (diffInSeconds < 3600) {
-			const minutes = Math.floor(diffInSeconds / 60);
-			relativeTime = `${minutes}m ago`;
-		} else if (diffInSeconds < 86400) {
-			const hours = Math.floor(diffInSeconds / 3600);
-			relativeTime = `${hours}h ago`;
-		} else if (diffInSeconds < 604800) {
-			const days = Math.floor(diffInSeconds / 86400);
-			relativeTime = `${days}d ago`;
-		} else if (diffInSeconds < 2592000) {
-			const weeks = Math.floor(diffInSeconds / 604800);
-			relativeTime = `${weeks}w ago`;
-		} else if (diffInSeconds < 31536000) {
-			const months = Math.floor(diffInSeconds / 2592000);
-			relativeTime = `${months}mo ago`;
-		} else {
-			const years = Math.floor(diffInSeconds / 31536000);
-			relativeTime = `${years}y ago`;
-		}
-
-		const formattedDate = date1.toLocaleDateString('en-US', {
-			year: 'numeric',
-			month: 'long',
-			day: 'numeric',
-		});
-
-		return `${formattedDate} (${relativeTime})`;
-	}
 </script>
 
 <Card3D>
