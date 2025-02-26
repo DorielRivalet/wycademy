76ac19672d62eff3798ed955ecd6f823aa1d2a45
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2025-02-26 01:15:16 -0300

    feat: update copyright page

--- src/routes/support/(docs-full)/website/contact/+page.svelte
+++ src/routes/support/(docs-full)/website/contact/+page.svelte
@@ -3,12 +3,18 @@
 	import SectionHeadingTopLevel from '$lib/client/components/SectionHeadingTopLevel.svelte';
 	import TableOfContentsPage from '$lib/client/components/TableOfContentsPage.svelte';
 	import { page } from '$app/stores';
+	import OutboundLink from 'carbon-components-svelte/src/Link/OutboundLink.svelte';
 </script>
 
 <TableOfContentsPage displayTOC={false}>
 	<div>
 		<SectionHeadingTopLevel title={'Contact Us'} />
-		<div class="spaced-paragraph"></div>
+		<div class="spaced-paragraph">
+			If you have any issues or concerns, feel free to reach out to us through <OutboundLink
+				href="https://github.com/DorielRivalet/wycademy/issues"
+				>GitHub's issue tracker.</OutboundLink
+			>
+		</div>
 		<div class="page-turn">
 			<PageTurn pageUrlPathName={$page.url.pathname} />
 		</div>
