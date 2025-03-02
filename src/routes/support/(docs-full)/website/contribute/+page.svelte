ffac9160a6ad44c0a25342da2393c45c59a91ee9
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2025-03-01 23:03:50 -0300

    feat: update support pages

--- src/routes/support/(docs-full)/website/contribute/+page.svelte
+++ src/routes/support/(docs-full)/website/contribute/+page.svelte
@@ -3,12 +3,24 @@
 	import SectionHeadingTopLevel from '$lib/client/components/SectionHeadingTopLevel.svelte';
 	import TableOfContentsPage from '$lib/client/components/TableOfContentsPage.svelte';
 	import { page } from '$app/stores';
+	import OutboundLink from 'carbon-components-svelte/src/Link/OutboundLink.svelte';
 </script>
 
 <TableOfContentsPage displayTOC={false}>
 	<div>
-		<SectionHeadingTopLevel title={'Contribute to Wycademy'} />
-		<div class="spaced-paragraph"></div>
+		<SectionHeadingTopLevel title={'Contributions & Support'} />
+		<div class="spaced-paragraph">
+			Wycademy is still in active development, and many core aspects are
+			changing frequently. Because of this, we are holding off on formal
+			contribution guidelines for now. Once the site reaches v1.0, we will
+			provide clear instructions for those who want to contribute.
+		</div>
+		<div class="spaced-paragraph">
+			If you have feedback, bug reports, or ideas, feel free to <OutboundLink
+				href="https://github.com/DorielRivalet/wycademy/issues"
+				>open an issue on GitHub.</OutboundLink
+			>
+		</div>
 		<div class="page-turn">
 			<PageTurn pageUrlPathName={$page.url.pathname} />
 		</div>
