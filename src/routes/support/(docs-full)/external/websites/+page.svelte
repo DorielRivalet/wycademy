4edf3d45305fb065aea3361df7f3fff0ddc375c8
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2025-03-12 22:14:15 -0300

    feat: update external websites page

--- src/routes/support/(docs-full)/external/websites/+page.svelte
+++ src/routes/support/(docs-full)/external/websites/+page.svelte
@@ -6,22 +6,55 @@
 	import UnorderedList from 'carbon-components-svelte/src/UnorderedList/UnorderedList.svelte';
 	import ListItem from 'carbon-components-svelte/src/ListItem/ListItem.svelte';
 	import OutboundLink from 'carbon-components-svelte/src/Link/OutboundLink.svelte';
+	import SectionHeading from '$lib/client/components/SectionHeading.svelte';
+	import InlineNotification from 'carbon-components-svelte/src/Notification/InlineNotification.svelte';
 </script>
 
 <TableOfContentsPage displayTOC={false}>
 	<div>
 		<SectionHeadingTopLevel title={'Websites'} />
-		<UnorderedList class="spaced-list">
-			<ListItem
-				><p>
-					<OutboundLink href="https://mezehub.github.io/index.html"
-						>Mezehub</OutboundLink
-					>: a central source of resources for the MezeLounge (MHF-Z) private
-					server. It contains various guides and resources split across sections
-					for all levels of players.
-				</p></ListItem
+
+		<InlineNotification
+			lowContrast
+			hideCloseButton
+			kind="info"
+			title="Info:"
+			subtitle="If you wish to add or remove from this list, see our GitHub issue tracker
+			and select the External Tools template."
+		/>
+		<p class="spaced-paragraph">
+			<strong
+				>If your website represents a private server, we ask the following of
+				your community:</strong
 			>
+		</p>
+		<UnorderedList class="spaced-list">
+			<ListItem><p>Avoid excessive pay-to-win advantages.</p></ListItem>
+			<ListItem><p>Foster a non-toxic environment.</p></ListItem>
 		</UnorderedList>
+		<p class="spaced-paragraph">
+			We understand that managing communities is challenging, so these are
+			strong recommendations rather than strict requirements. However, we
+			reserve the right to curate and update this list as needed to maintain its
+			integrity.
+		</p>
+		<section>
+			<SectionHeading level={2} title="Private Servers" />
+			<div>
+				<UnorderedList class="spaced-list">
+					<ListItem
+						><p>
+							<OutboundLink href="https://mezehub.github.io/index.html"
+								>Mezehub</OutboundLink
+							>: a central source of resources for the MezeLounge (MHF-Z)
+							private server. It contains various guides and resources split
+							across sections for all levels of players.
+						</p></ListItem
+					>
+				</UnorderedList>
+			</div>
+		</section>
+
 		<div class="page-turn">
 			<PageTurn pageUrlPathName={$page.url.pathname} />
 		</div>
